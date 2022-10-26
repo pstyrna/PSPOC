0be2be849182edbac866b5adcca318df0f3f1795
Pawel Styrna <pawel.styrna@glantus.com>
2022-10-26 12:40:10 +0200

    update packeges

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,13 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ServiceWorkerModule } from '@angular/service-worker';
-import { PushNotificationsModule } from 'ng-push-ivy';
 import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AreYouSureComponent } from './components/are-you-sure/are-you-sure.component';
 import { TopBarComponent } from './components/top-bar/top-bar.component';
 import { WhoYouAreComponent } from './components/who-you-are/who-you-are.component';
 import { MaterialModule } from './shared/material.module';
-import { AreYouSureComponent } from './components/are-you-sure/are-you-sure.component';
 
 @NgModule({
     declarations: [AppComponent, TopBarComponent, WhoYouAreComponent, AreYouSureComponent],
@@ -21,7 +20,6 @@ import { AreYouSureComponent } from './components/are-you-sure/are-you-sure.comp
         BrowserAnimationsModule,
         MaterialModule,
         FormsModule,
-        PushNotificationsModule,
     ],
     providers: [],
     bootstrap: [AppComponent, TopBarComponent],
--- src/app/shared/browser-push.service.ts
+++ src/app/shared/browser-push.service.ts
@@ -1,21 +1,50 @@
 import { Injectable } from '@angular/core';
-import { Router } from '@angular/router';
-import { PushNotificationsService } from 'ng-push-ivy';
+import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class BrowserPushService {
-    constructor(private pushNotificationsService: PushNotificationsService, private router: Router) {}
+    private permission: NotificationPermission;
+    constructor() {
+        this.permission = this.isSupported() ? Notification.permission : 'denied';
+    }
 
     subscribeForPush(): void {
-        this.pushNotificationsService.requestPermission();
+        if ('Notification' in window) {
+            Notification.requestPermission((status) => (this.permission = status));
+        }
     }
 
     send(message: string, url: string): void {
-        this.pushNotificationsService.create(message).subscribe((res) => {
+        this.create(message).subscribe((res) => {
             if (res.event.type === 'click') {
                 res.notification.close();
                 window.open(window.location.origin + url);
             }
         });
+    }
+
+    private create(title: string): Observable<{ notification: Notification; event: any }> {
+        return new Observable((obs: any) => {
+            if (!('Notification' in window)) {
+                obs.error('Notifications are not available in this environment');
+                obs.complete();
+            }
+
+            if (this.permission !== 'granted') {
+                obs.error(`The user hasn't granted you permission to send push notifications`);
+                obs.complete();
+            }
+
+            const n = new Notification(title);
+
+            n.onshow = (e: any) => obs.next({ notification: n, event: e });
+            n.onclick = (e: any) => obs.next({ notification: n, event: e });
+            n.onerror = (e: any) => obs.error({ notification: n, event: e });
+            n.onclose = () => obs.complete();
+        });
+    }
+
+    private isSupported() {
+        return 'Notification' in window;
     }
 }
